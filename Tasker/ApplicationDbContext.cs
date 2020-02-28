fb1b900fd21b563d8f05993a36ea2fe7ce26fefa
Roozendaal, Ed <ed.roozendaal@INHolland.nl>
2020-02-28 08:31:05 +0100

    Module: Saving Data to the Database

---
+++ Tasker/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tasker
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        private ApplicationDbContext _context;
+
+        public CreateTaskModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [BindProperty]
         public Task NewTask { get; set; }
 
@@ -19,6 +22,8 @@ namespace Tasker.Pages
 
         public IActionResult OnPost()
         {
+            _context.Tasks.Add(NewTask);
+            _context.SaveChanges();
             return RedirectToPage("Index");
         }
     }

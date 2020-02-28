d2ad26f0330315ca921702ba4e9b86c79d301248
Roozendaal, Ed <ed.roozendaal@INHolland.nl>
2020-02-28 08:23:31 +0100

    .

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Tasker
+namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
